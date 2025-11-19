fc2e360b77e271505101054e81da4fd5f2d5ab64
Ma Hai Dang <haidang18101810@gmail.com>
2025-11-19 10:12:07 +0700

    chore: remove ignored fe

--- ecommerce-fe/app/(protected)/customer/products/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { BackButton } from "@/components/ui/BackButton";
-import CustomerProductDetail from "@/features/products/customer/CustomerProductDetail";
-
-export default function Page() {
-
-    return <CustomerProductDetail />;
-}
